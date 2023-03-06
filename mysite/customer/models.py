31363b18745f92cf250636eb8114c968585ef006
jananga99 <jananga1999@gmail.com>
2023-03-06 22:49:35 +0530

    Customer model added

--- mysite/customer/models.py
+++ mysite/customer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    firstName = models.CharField('First Name', max_length=200)
+    lastName = models.CharField('Last Name', max_length=200)
+    dob = models.DateField('Date of Birth')
+    currencyBalance = models.FloatField('Currency Balance', default=0)
+    pageVisits = models.IntegerField('Page Visits', default=0)
