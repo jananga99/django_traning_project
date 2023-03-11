b3e998638b01cb6cf2179d1047f70789cf68652d
jananga99 <jananga1999@gmail.com>
2023-03-11 13:23:16 +0530

    Expose Rest API

--- mysite/customer/models.py
+++ mysite/customer/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 
 class Customer(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
     firstName = models.CharField('First Name', max_length=200)
     lastName = models.CharField('Last Name', max_length=200)
     dob = models.DateField('Date of Birth')
     currencyBalance = models.FloatField('Currency Balance', default=0)
     pageVisits = models.IntegerField('Page Visits', default=0, validators=[MinValueValidator(0)])
+
+    class Meta:
+        ordering = ['created']
