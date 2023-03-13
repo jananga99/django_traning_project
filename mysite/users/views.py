aa96ce76d890c7b9865c61b6ee00db3e986b6666
jananga99 <jananga1999@gmail.com>
2023-03-13 08:46:23 +0530

    Signin Signup partially improved

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,8 +1,12 @@
 import requests
+from django.http import JsonResponse
 from django.shortcuts import render
 import pyrebase
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 config = {
@@ -30,19 +34,27 @@ def home(request):
 
 
 def postsignIn(request):
+    print("Start of post request")
+    print(request.POST['email'])
     email = request.POST.get('email')
     pasw = request.POST.get('password')
     try:
         # if there is no error then signin the user with given email and password
         user = authe.sign_in_with_email_and_password(email, pasw)
+        print(user)
         session_id = user['idToken']
         request.session['uid'] = str(session_id)
-        return render(request, "Home.html", {"email": email})
+        response_data = {
+            'session_id': session_id,
+            'email': email,
+        }
+        return JsonResponse(response_data)
+        # return render(request, "Home.html", {"email": email})
     except Exception as e:
         message = "An error occurred while signing in. Please try again later."
         print(e)
-        return render(request, "Signin.html", {"message": message})
-
+        return JsonResponse({'error': message})
+        # return render(request, "Signin.html", {"message": message})
 
 
 def logout(request):
@@ -56,18 +68,28 @@ def logout(request):
 def signUp(request):
     return render(request, "Signup.html")
 
-
 def postsignUp(request):
-    email = request.POST.get('email')
-    passs = request.POST.get('password')
-    name = request.POST.get('name')
+    print("In begiuning")
+    data = JSONParser().parse(request)
+    print(data)
+    email = data.get('email')
+    passs = data.get('password')
+    name = data.get('username')
+    print(email, passs, name)
     try:
         # creating a user with the given email and password
         user = authe.create_user_with_email_and_password(email, passs)
         print(user)
         uid = user['localId']
         print(uid)
+        response_data = {
+            'uid': uid,
+            'email': email,
+        }
+        return JsonResponse(response_data)
     except Exception as e:
         print(e)
-        return render(request, "Signup.html")
-    return render(request, "Signin.html")
+        message = "Sign up error"
+        return JsonResponse({'error': message})
+        # return render(request, "Signup.html")
+    # return render(request, "Signin.html")
